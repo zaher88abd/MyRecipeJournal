02e7eeb910c92080ce686127e747b359c23a376d
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-02-21 15:33:32 -0400

    Add toolBar

--- app/src/main/java/ca/zaher/m/myrecipejournal/MainActivity.java
+++ app/src/main/java/ca/zaher/m/myrecipejournal/MainActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,6 +19,7 @@ import java.net.InetAddress;
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser user;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
         if (user != null) {
             setTitle(user.getEmail().split("@")[0]);
         }
@@ -34,6 +40,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     @Override
