a74df4bd815dedd52217fa4d4fd2682ad83de312
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-02-20 22:20:02 -0400

    Creat sign up lay out and add firebase auth

--- app/src/main/java/ca/zaher/m/myrecipejournal/MainActivity.java
+++ app/src/main/java/ca/zaher/m/myrecipejournal/MainActivity.java
@@ -3,11 +3,27 @@ package ca.zaher.m.myrecipejournal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup_user);
+        mAuth = FirebaseAuth.getInstance();
     }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+//    }
+//
+//    private void updateUI(FirebaseUser currentUser) {
+//
+//    }
 }
