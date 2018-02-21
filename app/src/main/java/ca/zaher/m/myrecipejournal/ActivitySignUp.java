7dbeab33673291a7da0534f6daef7f0188b43132
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-02-21 12:31:40 -0400

    Create sigin up
    Create sigin up activity
    add internet permission 
    test signup

---
+++ app/src/main/java/ca/zaher/m/myrecipejournal/ActivitySignUp.java
@@ -0,0 +1,117 @@
+package ca.zaher.m.myrecipejournal;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+/**
+ * Sign-up user interface
+ * Created by zaher on 2018-02-21.
+ */
+
+public class ActivitySignUp extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etCPassword;
+    private ProgressDialog progressDialog;
+    private Button btnSignUp;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.signup_user);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        etCPassword = findViewById(R.id.et_password_confirm);
+        progressDialog = new ProgressDialog(this);
+        btnSignUp = findViewById(R.id.btn_signup);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUp();
+            }
+        });
+    }
+
+    public void signUp() {
+        final String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String cPassword = etCPassword.getText().toString();
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(cPassword)) {
+            if (password.length() < 6) {
+                Toast.makeText(this, R.string.password_length_message, Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!password.equals(cPassword)) {
+                Toast.makeText(this, R.string.password_not_match, Toast.LENGTH_LONG).show();
+                return;
+            }
+            progressDialog.setMessage(getString(R.string.signup_wiate));
+            progressDialog.show();
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    progressDialog.cancel();
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = task.getResult().getUser();
+                        DatabaseReference userDbRef = FirebaseDatabase.getInstance()
+                                .getReference().child("user").child(user.getUid());
+                        HashMap<String, Object> map = new HashMap<String, Object>();
+                        map.put("email", email);
+                        userDbRef.setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                } else {
+                                    //user.delete();
+                                    Toast.makeText(ActivitySignUp.this,
+                                            "Could not add the user to the database", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    } else {
+                        Toast.makeText(ActivitySignUp.this,
+                                "Could not add the user", Toast.LENGTH_SHORT).show();
+                        Log.e("Firebase Error", task.getException().toString());
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.error_signup, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this, "Welcome, " + email.split("@")[0], Toast.LENGTH_SHORT).show();
+        backToMainActivity();
+    }
+
+    private void backToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
+//Referance
+//http://melardev.com/eng/blog/2018/01/02/android-firebase-register-users/
--- app/src/main/java/ca/zaher/m/myrecipejournal/MainActivity.java
+++ app/src/main/java/ca/zaher/m/myrecipejournal/MainActivity.java
@@ -1,10 +1,15 @@
 package ca.zaher.m.myrecipejournal;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -12,18 +17,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup_user);
+        setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
     }
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        updateUI(currentUser);
-//    }
-//
-//    private void updateUI(FirebaseUser currentUser) {
-//
-//    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser == null) {
+            Intent intent = new Intent(this, ActivitySignUp.class);
+            startActivity(intent);
+        }
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null;
+    }
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("google.com");
+            //You can replace it with your name
+            return !ipAddr.equals("");
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
+//https://stackoverflow.com/questions/9570237/android-check-internet-connection
