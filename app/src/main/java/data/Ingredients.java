40510403919e1f387b88448d373026fd1da0016c
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-02-22 14:49:48 -0400

    Add Getter Sitter

--- app/src/main/java/data/Ingredients.java
+++ app/src/main/java/data/Ingredients.java
@@ -9,4 +9,33 @@ class Ingredients {
     float quantity;
     String measurement;
 
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasurement() {
+        return measurement;
+    }
+
+    public void setMeasurement(String measurement) {
+        this.measurement = measurement;
+    }
+
+    public Ingredients(String material, float quantity, String measurement) {
+        this.material = material;
+        this.quantity = quantity;
+        this.measurement = measurement;
+    }
 }
--- app/src/main/java/data/Recipe.java
+++ app/src/main/java/data/Recipe.java
@@ -12,4 +12,43 @@ public class Recipe {
     private String description;
     private List<Ingredients> ingredientsList;
     private String instructions;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Ingredients> getIngredientsList() {
+        return ingredientsList;
+    }
+
+    public void setIngredientsList(List<Ingredients> ingredientsList) {
+        this.ingredientsList = ingredientsList;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public Recipe(String name, String description, List<Ingredients> ingredientsList, String instructions) {
+        this.name = name;
+        this.description = description;
+        this.ingredientsList = ingredientsList;
+        this.instructions = instructions;
+    }
 }
