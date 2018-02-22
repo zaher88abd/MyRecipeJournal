b29d01d45692fcb98538c08f539634dd35ffcae2
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-02-22 14:47:46 -0400

    Create Class
    Create the class to save the Recipe data of it.

---
+++ app/src/main/java/data/Ingredients.java
@@ -0,0 +1,12 @@
+package data;
+
+/**
+ * Created by zaher on 2018-02-22.
+ */
+
+class Ingredients {
+    String material;
+    float quantity;
+    String measurement;
+
+}
---
+++ app/src/main/java/data/Recipe.java
@@ -0,0 +1,15 @@
+package data;
+
+import java.util.List;
+
+/**
+ * the class of the Recipe data
+ * Created by zaher on 2018-02-22.
+ */
+
+public class Recipe {
+    private String name;
+    private String description;
+    private List<Ingredients> ingredientsList;
+    private String instructions;
+}
